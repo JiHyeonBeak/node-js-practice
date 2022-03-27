aae6fa92474f3d0126301e8bb3917d1dcd6e3b9d
JiHyeonBeak <jadehl09@gmail.com>
2022-03-27 21:06:43 +0900

    callback function 1

--- node_example1.js
+++ node_example1.js
@@ -1,1 +1,12 @@
-console.log("initial node!");
+console.log("initial node!");   // node node_example1.js
+
+function add(a, b, callback) {  // Js콜백 함수 샘플
+    var result = a + b;
+    callback(result);
+}
+
+add(10, 10, function(result) {
+    console.log("파라미터로 전달 된 콜백 함수 호출");
+    console.log("결과 : "+result);
+})
+
